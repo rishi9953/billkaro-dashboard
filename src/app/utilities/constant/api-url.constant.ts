5b4c315ef004ceb454e29b2b19d8984b8b00c9a9
rishi9953 <rk5680179@gmail.com>
2026-04-13 14:25:59 +0530

    url changes

--- src/app/utilities/constant/api-url.constant.ts
+++ src/app/utilities/constant/api-url.constant.ts
@@ -4,8 +4,8 @@
  * For local development, it also uses the direct URL
  */
 export const API_BASE_URL = 
-// 'https://65.2.81.212/api';
-  'https://nmsmfkdd-3000.inc1.devtunnels.ms/api';
+'https://65.2.81.212/api';
+  // 'https://nmsmfkdd-3000.inc1.devtunnels.ms/api';
 
 // 'https://65.2.81.212/api'
 
